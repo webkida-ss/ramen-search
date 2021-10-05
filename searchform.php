53dd442b1bdc7c798208c1ee966af223b6257f20
webkida-ss <webkida.production@gmail.com>
2021-10-05 23:01:58 +0900

    top作成

--- searchform.php
+++ searchform.php
@@ -1,1 +1,10 @@
 <!-- 検索画面 -->
+<div class="searchform">
+	<p>フリーワードで検索する</p>
+	<form role="search" method="get" action="<?php echo esc_url(home_url() . '/'); ?>">
+		<input type="text" name='s'>
+		<button type="submit">
+			<img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/ramen_shoyu_small.png'); ?>" alt="検索ボタン">
+		</button>
+	</form>
+</div>
---
+++ src-php/20_component/01_top/_top.php
@@ -0,0 +1,7 @@
+<div class="top">
+	<div class="inner top__container">
+		<div class="top__search">
+			<?php get_search_form(); ?>
+		</div>
+	</div>
+</div>
