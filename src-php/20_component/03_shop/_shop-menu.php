86dd7ec7ebb2a62a512bedb13785c47515cdcefc
webkida-ss <webkida.production@gmail.com>
2021-10-07 23:05:03 +0900

    店舗作成

--- src-php/20_component/03_shop/_shop-menu.php
+++ src-php/20_component/03_shop/_shop-menu.php
@@ -1,8 +1,17 @@
-<!-- データ -->
-<?php
-
-?>
-
-<!-- 本文 -->
 <div class="shop-menu">
+	<div class="shop-menu__container">
+		<?php if (have_rows('shop_menu')) : ?>
+			<?php while (have_rows('shop_menu')) : the_row(); ?>
+				<div class="shop-menu__item">
+					<img src="<?php the_sub_field('shop_menu_img'); ?>" alt="メニュー画像">
+					<div>
+						<?php the_sub_field('shop_menu_title'); ?>
+						<br>
+						<span><?php the_sub_field('shop_menu_price'); ?></span>円
+					</div>
+					<p><?php the_sub_field('shop_menu_feature'); ?></p>
+				</div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
 </div>
--- src-php/20_component/03_shop/_shop-top.php
+++ src-php/20_component/03_shop/_shop-top.php
@@ -1,10 +1,19 @@
-<!-- データ -->
-<?php
-
-?>
-
-<!-- 本文 -->
 <div class="shop-top">
-
-
+	<div class="shop-top__top-msg"><?php the_field('shop_top_msg'); ?></div>
+	<div class="shop-top__menu">
+		<div class="shop-top__main">
+			<?php the_post_thumbnail(); ?>
+		</div>
+		<div class="shop-top__sub">
+			<?php if (have_rows('shop_menu')) : ?>
+				<?php while (have_rows('shop_menu')) : the_row(); ?>
+					<div class="shop-top__sub--item">
+						<img src="<?php the_sub_field('shop_menu_img'); ?>" alt="メニュー画像">
+						<p><?php the_sub_field('shop_menu_title'); ?></p>
+						<p><span><?php the_sub_field('shop_menu_price'); ?></span>円</p>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+	</div>
 </div>
