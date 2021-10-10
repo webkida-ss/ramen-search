86370a2b70dc355f51a6ca5db24c5f3bae0ad177
webkida-ss <webkida.production@gmail.com>
2021-10-10 21:55:33 +0900

    404修正

--- 404.php
+++ 404.php
@@ -1,1 +1,36 @@
 <!-- エラー画面 -->
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<?php get_header(); ?>
+</head>
+
+<body>
+	<div class="wrapper front-page">
+
+		<!-------------------------------------------------- 
+		- ヘッダー
+		---------------------------------------------------->
+		<?php get_template_part('src-php/10_common/_header'); ?>
+
+
+		<!-------------------------------------------------- 
+		- メイン
+		---------------------------------------------------->
+		<div class="container inner">
+			<main class="main">
+				<p style="text-align: center;">該当のページは存在しません。</p>
+			</main>
+		</div><!-- /.container -->
+
+		<!-------------------------------------------------- 
+		- フッター
+		---------------------------------------------------->
+		<?php get_template_part('src-php/10_common/_footer'); ?>
+	</div><!-- /.wrapper -->
+
+	<?php get_footer(); ?>
+</body>
+
+</html>
